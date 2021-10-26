dc8d2df19db936c4d403bf77be737ae01ea6e752
Administrator <test@test.ro>
2021-10-26 08:10:55 +0300

    added flooring equipment

---
+++ src/main/java/Equipment/groups/FlooringEquipment.java
@@ -0,0 +1,10 @@
+package Equipment.groups;
+
+import Equipment.Equipment;
+
+public class FlooringEquipment extends Equipment {
+    @Override
+    public void setType() {
+        this.type = "Flooring";
+    }
+}
