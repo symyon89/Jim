c81289ad86085ab50d0fc6525e7f070bee08ee96
Administrator <test@test.ro>
2021-10-26 09:08:09 +0300

    added flooring types

---
+++ src/main/java/Equipment/FlooringTypes/FlooringMatsEquipment.java
@@ -0,0 +1,9 @@
+package Equipment.FlooringTypes;
+
+import Equipment.groups.FlooringEquipment;
+
+public class FlooringMatsEquipment extends FlooringEquipment {
+    public void setType() {
+        this.type = "Flooring Mats";
+    }
+}
---
+++ src/main/java/Equipment/FlooringTypes/InterlockingEquipment.java
@@ -0,0 +1,9 @@
+package Equipment.FlooringTypes;
+
+import Equipment.groups.FlooringEquipment;
+
+public class InterlockingEquipment extends FlooringEquipment {
+    public void setType() {
+        this.type = "Interlocking";
+    }
+}
